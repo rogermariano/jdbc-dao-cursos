84c090950f94d947c50b1e7859210f024a2810b1
rogermariano <prof_rogerio_mariano@hotmail.com>
2020-02-09 20:53:35 -0300

    Classe de entidade Departamento

---
+++ src/application/Program01.java
@@ -0,0 +1,15 @@
+package application;
+
+import model.entidade.Departamento;
+
+public class Program01 {
+
+	public static void main(String[] args) {
+
+		Departamento depto = new Departamento(1, "Computadores");
+		
+		System.out.println("Departamento ===> " + depto);
+		
+	}
+
+}
